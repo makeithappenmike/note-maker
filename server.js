8847635a9e6081f001e3845f97d0daff46d3c9b8
MakeithappenMike <omichaelaaron@gmail.com>
2022-09-15 19:43:15 -0400

    get notes is working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const uuid = require('./helper/uuid');
 
 // Needed for Heroku
 const PORT = process.env.PORT || 3001;
@@ -14,18 +14,18 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for notes.jtml
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.listen(PORT, () =>
