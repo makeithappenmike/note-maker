b0b187bb75e287a00b73f8bdf9dd6ee0db407e8e
MakeithappenMike <omichaelaaron@gmail.com>
2022-09-19 15:47:48 -0400

    adding fallback

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const uuid = require('../helper/uuid');
 const { readFromFile, readAndAppend } = require('../helper/fsUtils');
 const fs = require('fs');
+const db = require('../db/db.json');
 
 // Bring in Routes
 // const apiRoutes = require('./apiRoutes.js');
@@ -12,16 +13,49 @@ const app = express();
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
+// Fallback route for when a user attempts to visit routes that don't exist
+app.get('*', (req, res) =>
+  res.send(
+    `Make a GET request using to <a href="http://localhost:${PORT}/api/notes">http://localhost:${PORT}/api/notes</a>`
+  )
+);
+
 // GET Route for retrieving all Notes
 app.get('/notes', (req, res) => {
   console.info(`${req.method} request received for notes on api.js`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// DELETE Route for delete a note
-app.get('/notes', (req, res) => {
-  console.info(`${req.method} request received for notes on api.js`);
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// GET route that returns a note by id
+app.get('/api/notes/:id', (req, res) => {
+  // Coerce the specific search term to lowercase
+  const idQueried = req.params.id.toLowerCase();
+
+  // Iterate through the terms name to check if it matches `req.params.term`
+  for (let i = 0; i < db.length; i++) {
+    if (idQueried === db[i].id.toLowerCase()) {
+      return res.json(db[i]);
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json('No match found');
+});
+
+// DELETE route
+app.delete('/api/notes/:id', (req, res) => {
+  // Coerce the specific search term to lowercase
+  const requestedTerm = req.params.term.toLowerCase();
+
+  // Iterate through the terms name to check if it matches `req.params.term`
+  for (let i = 0; i < termData.length; i++) {
+    if (requestedTerm === termData[i].term.toLowerCase()) {
+      return res.json(termData[i]);
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json('No match found');
 });
 
 // POST Route for submitting notes
@@ -33,7 +67,6 @@ app.post('/notes', (req, res) => {
     if (req) {
         const title = req.body.title;
         const text = req.body.text;
-        const allNotes =  [];
         const id = uuid();
 
         // Create noteAdded object
@@ -43,7 +76,6 @@ app.post('/notes', (req, res) => {
             text: text
         };
 
-        // console.log("Note Type:", typeof(JSON.stringify(noteAdded)));
         console.log("Note Added:", noteAdded);
         
         readAndAppend(noteAdded, './db/db.json');
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,16 +4,16 @@ const uuid = require('../helper/uuid');
 const { readFromFile, readAndAppend } = require('../helper/fsUtils');
 
 
-// GET Route for homepage
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-  console.info(`${req.method} request received for index from htmlRoutes.js`);
-});
+// // GET Route for homepage
+// router.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+//   console.info(`${req.method} request received for index from htmlRoutes.js`);
+// });
 
-// GET Route for notes.html
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-  console.info(`${req.method} request received for notes on htmlRoutes.js`);
-});
+// // GET Route for notes.html
+// router.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/notes.html'));
+//   console.info(`${req.method} request received for notes on htmlRoutes.js`);
+// });
 
 module.exports = router;
